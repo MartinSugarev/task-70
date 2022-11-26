2970732c8d7d869db85f864a2e88b3b00b225e40
Martin <mdsugarev@gmail.com>
2022-11-26 23:50:03 +0200

    added click event

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,11 @@
-import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+
+  const img = document.querySelector('.image')
+  img.addEventListener('click', () => {
+    img.style.transform = 'scale(2, 2)'
+  })
 
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
